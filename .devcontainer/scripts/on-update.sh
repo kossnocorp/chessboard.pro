3318102035398a37ee96525b2dc090fa95f4eb7c
Sasha Koss <koss@nocorp.me>
2025-09-13 20:32:26 +0000

    Set up dev container, turn into a monorepo

---
+++ .devcontainer/scripts/on-update.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# This script is when the container is updated.
+
+set -e
+
+source ~/.config/mothership/.env || true
+
+# Make sure mise is activated
+export PATH="$HOME/.local/bin:$PATH"
+command -v mise >/dev/null || { echo "🔴 mise not found in $HOME/.local/bin"; exit 1; }
+eval "$(mise activate bash --shims)"
+
+# Pull git submodules
+if [ -d .git ]; then
+  git submodule update --recursive --init --remote
+fi
+
+# Trust all mise configs
+mise trust --yes --all
+if [ -d .git ]; then
+  git submodule foreach --recursive "mise trust"
+fi
+
+# Update mise
+mise self-update -y
+
+# Install stack
+mise install
+
+# Install dependencies
+if [ -f ./pnpm-lock.yaml ]; then
+  yes | pnpm install
+elif [ -f ./yarn.lock ]; then
+  yes | yarn install
+elif [ -f ./package-lock.json ]; then
+  yes | npm install
+fi
+
+# Install dependencies
+if [ -f ./Cargo.lock ]; then
+  cargo build || echo "🟡 Cargo build failed, but that's ok"
+fi
